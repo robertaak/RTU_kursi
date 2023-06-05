dbce4a9de06e23fc988f136f04c0db601c01c9f5
Roberta <r.kimerale@gmail.com>
2023-06-05 20:43:21 +0300

    Initial commit

---
+++ dienas_uzdevumi.py
@@ -0,0 +1,20 @@
+# Izdrukājiet savu vārdu 5 reizes.
+my_name = "Roberta "
+print(my_name * 5)
+
+# Kas notiks ja mēģināsiet reizināt savu vārdu ar kādu pozitīvu skaitli, piem. 7 ?
+print(my_name * 7)
+
+# Aprēķiniet cik sekundes būs šogad (365 dienas šogad!)
+days = 365
+seconds_per_day = 60**2 * 24
+
+result = seconds_per_day * days
+print(result)
+
+# Aprēķiniet googol, tas ir 10 - 100tajā pakāpē (10**100)
+googol = 10**100
+print(googol)
+
+# Izdrukājiet Banananana - tikai izmantojot Ba un na (nevis pilnu vārdu)
+print("Ba" + "na" * 4)
